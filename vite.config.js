087093babd0d5817d0fa3cb6861a31227296fce6
Oyindamola Joshua Akanbi <ojakanbi@oyindamolas-mbp.mynetworksettings.com>
2026-02-13 17:05:22 -0500

    recent

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/dj-kalz-website/', // Change this to your repo name
+  base: '/djkalzwebsite/', // Change this to your repo name
 })
